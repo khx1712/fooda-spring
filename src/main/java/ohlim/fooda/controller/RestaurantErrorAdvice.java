fa592ad60ad8e6dea733cfe5d877362172812940
오승찬 <khx1712@naver.com>
2020-10-02 23:46:52 +0900

    add restaurantNotFoundHandler

--- src/main/java/ohlim/fooda/controller/RestaurantErrorAdvice.java
+++ src/main/java/ohlim/fooda/controller/RestaurantErrorAdvice.java
@@ -1,20 +1,30 @@
 package ohlim.fooda.controller;
 
 import ohlim.fooda.service.RestaurantNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class RestaurantErrorAdvice {
+    private Logger log = LoggerFactory.getLogger(ApplicationRunner.class);
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(RestaurantNotFoundException.class)
-    public String handlerNotFound(){
-        return "{}";
+    public Map<String, String> handlerNotFound(RestaurantNotFoundException e){
+        log.error(e.getMessage(), e);
+        Map<String, String> errorAttributes = new HashMap<>();
+        errorAttributes.put("code", "RESTAURANT_NOT_FOUND");
+        errorAttributes.put("message", e.getMessage());
+        return errorAttributes;
     }
-
 }
